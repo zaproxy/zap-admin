76222395101c4f1bf7b0397e95ae31cec560bf3a
Simon Bennetts <psiinon@gmail.com>
2023-10-09 17:39:15 +0200

    Add 2.14 files
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,7 +107,7 @@ val latestZapVersions = file(nameLatestZapVersions)
 val ghUser = GitHubUser("zapbot", "12745184+zapbot@users.noreply.github.com", System.getenv("ZAPBOT_TOKEN"))
 val adminRepo = GitHubRepo("zaproxy", "zap-admin", rootDir)
 
-val addOnsZapVersions = files(devZapVersions, latestZapVersions)
+val addOnsZapVersions = files(devZapVersions, latestZapVersions, "ZapVersions-2.14.xml")
 val defaultChecksumAlgorithm = "SHA-256"
 
 tasks {
